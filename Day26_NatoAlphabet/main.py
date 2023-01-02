18946d316aff6c0c57a2a7b6982032f05be8b0d4
Regina <regina.crespo.018@gmail.com>
2023-01-02 17:50:57 -0600

    Fixed bug in day 26: Not working with spaces.

--- Day26_NatoAlphabet/main.py
+++ Day26_NatoAlphabet/main.py
@@ -7,7 +7,7 @@ nato_dict={row.letter:row.code for (index,row) in nato_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-input_word=input("Enter a word")
+input_word=input("Enter a word").strip()
 
 code_word=[nato_dict[letter.capitalize()] for letter in input_word]
 print(code_word)
