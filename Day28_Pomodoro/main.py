a5ae13276658c52e39202c5444858b6abdec895c
Regina <regina.crespo.018@gmail.com>
2023-01-15 17:42:32 -0600

    Day 28: Fixed reset button

--- Day28_Pomodoro/main.py
+++ Day28_Pomodoro/main.py
@@ -7,53 +7,59 @@ RED = "#f26849"
 GREEN = "#379b46"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 1
-LONG_BREAK_MIN = 1
-checkmark_text=""
-reps=1
-tempo=None
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+checkmark_text = ""
+reps = 1
+tempo = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
 def timer_reset():
     global reps
+    global checkmark_text
     title.config(text="Timer", fg=GREEN)
-    reps=0
-    checkmarks.config(text = "")
-    window.after_cancel(tempo) #Issue: for some reason it wont stop the timer
-    checkmarks.config(text = checkmark_text)
-    
+    reps = 0
+    checkmark_text=""
+    checkmarks.config(text="")
+    canvas.itemconfig(timer, text="00:00")
+    window.after_cancel(tempo)  # Its working now!
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
     global reps
-    work_sec=WORK_MIN*60
-    short_sec=SHORT_BREAK_MIN*60
-    long_sec=LONG_BREAK_MIN*60
+    work_sec = WORK_MIN*60
+    short_sec = SHORT_BREAK_MIN*60
+    long_sec = LONG_BREAK_MIN*60
 
     if reps == 9:
         timer_reset()
     elif reps == 8:
-        title.config(text="Long Break!",fg=PINK)
+        title.config(text="Long Break!", fg=PINK)
         count_down(long_sec)
     elif reps % 2 == 0:
-        title.config(text="Short Break!",fg=GREEN)
+        title.config(text="Short Break!", fg=GREEN)
         count_down(short_sec)
     else:
         title.config(text="Work Time!", fg=RED)
         count_down(work_sec)
 
+
 def increase_reps():
     global checkmark_text
     global reps
     if reps % 2 != 0:
-        checkmark_text=checkmark_text+"✔️"
+        checkmark_text = checkmark_text+"✔️"
     reps += 1
+
 
 def break_time():
     count_down(SHORT_BREAK_MIN*60)
+
 
 def raise_above_all():
     window.attributes('-topmost', 1)
@@ -62,10 +68,11 @@ def raise_above_all():
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
-def count_down(time): 
+def count_down(time):
+    global tempo
 
     minutes = int(time // 60)
-    seconds = int(time % 60) 
+    seconds = int(time % 60)
 
     if time > 0:
         if seconds < 10:
@@ -75,21 +82,22 @@ def count_down(time):
         if minutes < 10:
             minutes = "0" + str(minutes)
 
-        tempo=window.after(1000, count_down, time-1) 
+        tempo = window.after(1000, count_down, time-1)
         time_text = (f"{minutes}:{seconds}")
         canvas.itemconfig(timer, text=time_text)
 
     else:
-        time_text="00:00"
+        time_text = "00:00"
         increase_reps()
         window.lift()
         canvas.itemconfig(timer, text=time_text)
-        checkmarks.config(text = checkmark_text)
+        checkmarks.config(text=checkmark_text)
         raise_above_all()
         start_timer()
     canvas.itemconfig(timer, text=time_text)
-    
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = tkinter.Tk()
 window.title("Pomodoro Timer")
@@ -116,12 +124,12 @@ start_button = tkinter.Button(
     command=start_timer, text="Start", highlightthickness=0)
 start_button.grid(column=1, row=4)
 
-reset_button = tkinter.Button(command=timer_reset,text="Reset", highlightthickness=0)
+reset_button = tkinter.Button(
+    command=timer_reset, text="Reset", highlightthickness=0)
 reset_button.grid(column=3, row=4)
 
 checkmarks = tkinter.Label(
     text=checkmark_text, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, "bold"))
 checkmarks.grid(column=2, row=4)
-
 
 window.mainloop()
