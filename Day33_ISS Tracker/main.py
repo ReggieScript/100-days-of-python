72ab0032655888e768f1db9c5e170f1f987d2613
Regina <regina.crespo.018@gmail.com>
2023-01-26 11:29:39 -0600

    Day 33: Functional ISS tracker

--- Day33_ISS Tracker/main.py
+++ Day33_ISS Tracker/main.py
@@ -1,21 +1,41 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-my_lat = 25.6866130
-my_lon = -100.3161160
+my_lat = 0 #fill with your lat
+my_lon = 0 #fill wwith your long
+your_city = "" #fill with your city
+
+letter = f"Subject: Look up!\n\nThe International Space Station is right above {your_city}, look up!"
+
+list_mails = () #fill with a list of emails you would like to send to the notifications
+
+test_email = "" #Fill this with your email
+pwd_email = "" #Email password
 
 #obtaining ISS data
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+def iss_loc_api():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-response.raise_for_status()
+    response.raise_for_status()
 
-data = response.json()["iss_position"]
+    data = response.json()["iss_position"]
 
-latitude = data["latitude"]
-longitude = data["longitude"]
+    latitude = float(data["latitude"])
+    longitude = float(data["longitude"])
 
+    return latitude,longitude
 
+def send():
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=test_email, password=pwd_email)
+        for mail in list_mails:
+            connection.sendmail(from_addr=test_email, to_addrs=mail,
+                            msg=letter)
+        connection.close()
 
 #Obtaining time nighttime data
 
@@ -30,11 +50,16 @@ response = requests.get(url = "https://api.sunrisesunset.io/json?", params = par
 response.raise_for_status()
 data = response.json()
 
-print(data)
-# sunrise = data["results"]["sunrise"].split("T")[1]
-# sunset = data["results"]["sunset"].split("T")[1]
-
-# print(sunrise)
-# print(sunset)
+sunrise = int(data['results']['sunrise'].split(':')[0])
+sunset = int(data['results']['sunset'].split(':')[0]) + 12 # To set it to 24 hour time
 
 time_now = datetime.now()
+
+while time_now.hour >= sunset or time_now.hour <= sunrise:
+    time.sleep(120)
+    iss_loc = iss_loc_api()
+    latitude = iss_loc[0]
+    longitude = iss_loc[1]
+    if (latitude > my_lat - 5 and latitude < my_lat + 5) and (longitude > my_lon - 5 and longitude < my_lon + 5):
+        send()
+    time_now = datetime.now()
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ I thought of doing this README to keep track of projects and notes for each one 
 
 ## Day 28- Pomodoro Timer
 Building the GUI in tkinter was HARD. I feel like I can still optimize the code and improve the readability. 
-Issue: the restart button wont work. I'll try to check the after_cancel()method.
+Issue: the restart button wont work. I'll try to check the `after_cancel()` method.
 Update: I didn't declare the variable as global so that's why it didn't work!
 
 ## Day 29- Password Manager
@@ -14,3 +14,13 @@ Back in day 5 I built a passwor generator with more than 30 lines, and today, th
 
 ## Day 31- Flashcards (22/01/23)
 I finished the flashcard project! It wasn't so hard except for tkinter, settng up the GUI is always an issue. Anyway, it was a fun project.
+
+## Day 33 - ISS tracker (26/01/23)
+This was such a fun project! There's an issue which I haven't had the time to fix, where the program will send several times an email when the ISS is above you. 
+Anyway, to use it for your convenience, i had some variables to fill in with your own data. such as:
+`my_lat = 0 #fill with your lat`
+`my_lon = 0 #fill wwith your lon`
+`your_city = "" #fill with your city`
+`list_mails = () #fill with a list of emails you would like to send to the notifications`
+`test_email = "" #Fill this with your email from which you will send the notifications`
+`pwd_email = "" #Email password`
